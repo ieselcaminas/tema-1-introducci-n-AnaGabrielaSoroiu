78bc0cc95c7a1c310e406dc30dbe7e175dd1c013
anasor2 <anasor2@alu.edu.gva.es>
2024-09-20 12:46:25 +0200

    Ejercicio23

---
+++ src/Ejercicio24.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ejercicio24 {
+    public static void main(String[] args) {
+        int nota;
+        int CuantasNotas = 0;
+        float SumaNotas = 0;
+        boolean HayDiez = false;
+
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            nota = sc.nextInt();
+            if (nota != -1) {
+                SumaNotas += nota;
+                CuantasNotas++;
+                if (nota == 10) {
+                    HayDiez = true;
+                }
+            }
+        } while (nota != -1);
+
+        System.out.println("La suma de las notas es: " + (SumaNotas/CuantasNotas));
+
+        if (HayDiez) {
+            System.out.println("Hay un 10 en tus notas.");
+        }else {
+            System.out.println("No hay ningún 10 en tus notas.");
+        }
+
+    }
+
+}
