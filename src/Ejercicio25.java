6d233fc79e72bef574b57eef715deb257478562b
anasor2 <anasor2@alu.edu.gva.es>
2024-09-24 12:45:12 +0200

    Factorial

---
+++ src/Ejercicio25.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio25 {
+    public static void main(String[] args) {
+        long factorial = 1;
+        int num;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese un número: ");
+        num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println("El factorial de " + num + " es " + factorial);
+    }
+}
