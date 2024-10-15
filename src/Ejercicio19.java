fd5d890558e5b6a7c1618d7340ee7ba0ee7453b4
anasor2 <anasor2@alu.edu.gva.es>
2024-10-15 10:17:20 +0200

    Calcular salario neto

---
+++ src/Ejercicio19.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+    public class Ejercicio19 {
+        public static void main(String[] args) {
+            double PrecioHora;
+            int NumHoras;
+            double bruto;
+            double impuestos;
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Ingrese el precio por horas");
+            PrecioHora = sc.nextDouble();
+
+            System.out.println("Ingrese número de horas");
+            NumHoras = sc.nextInt();
+
+            if (NumHoras <= 35) {
+                bruto = NumHoras * PrecioHora;
+            } else {
+                bruto = 35 * PrecioHora + (NumHoras - 35) * PrecioHora * 1.5;
+            }
+            System.out.println("El salario bruto es " + bruto);
+
+            if (bruto <= 500) {
+                impuestos = 0;
+            } else if (bruto <= 900) {
+                impuestos = bruto - 500 * 0.25;
+            } else {
+                impuestos = (400 * 0.25) + (bruto - 500 - 400) * 0.45;
+            }
+
+            System.out.println("El salario bruto es " + bruto);
+            System.out.println("Los impuestos son " + impuestos);
+            System.out.println("El salario neto es " + (bruto - impuestos));
+
+        }
+    }
